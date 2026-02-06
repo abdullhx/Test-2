b4930fcc0d268c7dfcbf52b3784e3fabed1b136f
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-06 07:01:17 +0300

    Update WindUI loading URL in Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 -- تحميل WindUI
-local WindUI = loadstring(game:HttpGet('https://raw.githubusercontent.com/Footagesus/WindUI/refs/heads/main/main_example.lua'))()
+local WindUI = loadstring(game:HttpGet('https://raw.githubusercontent.com/Footagesus/WindUI/refs/heads/main/main.lua'))()
 
 -- تحميل الواجهة
 local WindowUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/abdullhx/Test-2/refs/heads/main/UI/Window.lua"))()
