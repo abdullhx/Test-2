dac63eb7d7441c32b6708260dbfa3d9272cc799b
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-06 06:49:56 +0300

    Add Toggle.lua for toggle functionality

---
+++ Elements/Toggle.lua
@@ -0,0 +1,9 @@
+return function()
+    return {
+        Title = "زر تبديل",
+        Default = false,
+        Callback = function(state)
+            print("Toggle:", state)
+        end
+    }
+end
